1f84639cb5d199f2ab8396fc3f43ab8e22388816
lishaobo <woqucc@hotmail.com>
2017-04-23 19:21:13 +0800

    日常

--- mymllib/main.cpp
+++ mymllib/main.cpp
@@ -12,23 +12,23 @@ using namespace std;
 using namespace myml;
 int main()
 {
-	//matrix_a<long double> m;
-	////ifstream f(R"(in.txt)", ios::in);
+	matrix_va<long double> m;
+	ifstream f(R"(in.txt)", ios::in);
 	////ifstream f(R"(E:\paper\feature\compound-10Mb-10ms-r1-q1000pa1\feature\feature0.txt)",ios::in);
 	////ifstream f(R"(D:\paper\features µ—È\cubic-10Mb-10ms-r1-q1000pa1\feature\feature0.txt)", ios::in);
-	//import_matrix_data(m, f);
+	import_matrix_data(m, f);
 	//auto x = m.sub_matrix(0, 0, m.size(), m.dimension());
 	//c45_tree tree(x);
 	//tree.train();
-	//cerr << m.size() << "," << m.dimension() << endl;
-	tree<string> tr;
-	auto root = tr.insert(tr.begin(),"ccc");
-	tr.append_child(root, "hello");
-	tr.append_child(root, "hello1");
-	tr.append_child(root, "hello2");
-	for (auto i = tr.begin_leaf(); i != tr.end_leaf(); i++)
-	{
-		cerr << *i << endl;
-	}
-
+	cerr << m.size() << "," << m.dimension() << endl;
+	//tree<string> tr;
+	//auto root = tr.insert(tr.begin(),"ccc");
+	//tr.append_child(root, "hello");
+	//tr.append_child(root, "hello1");
+	//tr.append_child(root, "hello2");
+	//for (auto i = tr.begin_leaf(); i != tr.end_leaf(); i++)
+	//{
+	//	cerr << *i << endl;
+	//}
+	return 0;
 }
